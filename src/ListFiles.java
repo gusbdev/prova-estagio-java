e05f802e5cca98023fdf7d6680f4ddd9d8a8fac9
Gustavo Barbosa <gusbdev@gmail.com>
2021-02-16 21:53:22 -0300

    2 exercises

---
+++ src/ListFiles.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+
+public class ListFiles {
+    public ListFiles(String directory) throws IOException {
+        File file = new File(directory);
+        File arrayFiles[] = file.listFiles();
+        int i = 0;
+        for(int j = arrayFiles.length;i < j; i++){
+            File files = arrayFiles[i];
+            System.out.println(files.getName());
+        }
+    }
+
+    public static void main(String[] args){
+        try{
+            ListFiles list = new ListFiles("/home/gus/IdeaProjects/prova-estagio-java");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
